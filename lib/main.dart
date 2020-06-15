8857d1c011ea79af1ce6eef9d0f4eee4ecfb29b7
VolkanMac <>
2020-06-15 18:15:11 +0300

    started

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import './screens/onboarding_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Onboarding UI',
+      debugShowCheckedModeBanner: false,
+      home: OnboardingScreen(),
+    );
+  }
+}
---
+++ lib/utilities/styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final kTitleStyle = TextStyle(
+    color: Colors.white,
+    fontFamily: 'CM Sans Serif',
+    fontSize: 26.0,
+    height: 1.3);
+
+final kSubtitleStyle = TextStyle(
+  color: Colors.white60,
+  fontSize: 18.0,
+  height: 1.3,
+);
